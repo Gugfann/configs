8578f4e3f8d6ba70d65b4422223086de65e7cee8
Kristian Hansen <kristian.hansen@dronevolt.com>
2023-06-18 23:10:49 +0200

    Add basic lunarvim configuration

---
+++ lunarvim/lvim/lua/user/keybindings.lua
@@ -0,0 +1,19 @@
+  -- Centers cursor when moving 1/2 page down and up
+  lvim.keys.normal_mode["<C-u>"] = "<C-u>zz"
+  lvim.keys.normal_mode["<C-d>"] = "<C-d>zz"
+
+  -- Yank into system clipboard
+  lvim.keys.visual_mode["<leader>y"] = '"+y'
+  lvim.keys.normal_mode["<leader>yy"] = '"+yy'
+  lvim.keys.normal_mode["<leader>yiw"] = '"+yiw'
+  lvim.keys.normal_mode["<leader>yiW"] = '"+yiW'
+  lvim.keys.normal_mode['<leader>yi"'] = '"+yi"'
+  lvim.keys.normal_mode['<leader>ya"'] = '"+ya"'
+  lvim.keys.normal_mode['<leader>yi('] = '"+yi('
+  lvim.keys.normal_mode['<leader>yi)'] = '"+yi)'
+  lvim.keys.normal_mode['<leader>yi}'] = '"+yi}'
+  lvim.keys.normal_mode['<leader>yi{'] = '"+yi{'
+  lvim.keys.normal_mode['<leader>yip'] = '"+yip'
+
+  -- Run script to merge compile commands from all ROS packages
+  lvim.keys.normal_mode["<leader>mc"] = ':!/workspace/src/robot_master/AI-Link/build_dependencies/merge_compile_commands.sh<CR><CR>'
---
+++ lunarvim/lvim/lua/user/settings.lua
@@ -0,0 +1,10 @@
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.filetype.add({
+    extension = {
+        launch = 'xml',
+        test = 'xml',
+    },
+})
