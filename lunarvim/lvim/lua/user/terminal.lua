389aec51f93cdb65d0ec4205e3a5da4f726989bc
Kristian Hansen <kmolha@gmail.com>
2024-01-18 22:00:34 +0100

    Fix terminal exit shortcut in lvim

--- lunarvim/lvim/lua/user/terminal.lua
+++ lunarvim/lvim/lua/user/terminal.lua
@@ -1,7 +1,7 @@
 local api = vim.api
 
-api.nvim_command("autocmd TermOpen * startinsert")             -- starts in insert mode
-api.nvim_command("autocmd TermOpen * setlocal nonumber norelativenumber")       -- no numbers
-api.nvim_command("autocmd TermEnter * setlocal signcolumn=no") -- no sign column
+api.nvim_command("autocmd TermOpen * startinsert")                        -- starts in insert mode
+api.nvim_command("autocmd TermOpen * setlocal nonumber norelativenumber") -- no numbers
+api.nvim_command("autocmd TermEnter * setlocal signcolumn=no")            -- no sign column
 
-vim.keymap.set('t', '<esc>', "<C-\\><C-n>")                    -- esc to exit insert mode
+vim.keymap.set('t', '<C-x>', "<C-\\><C-n>")                               -- esc to exit insert mode
